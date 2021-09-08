3b4ac396d2aeed59b0bdaa017b05e662640b8baf
Arttu Konttila <AB6254@Labranet.jamk.fi>
2021-09-08 13:55:34 +0300

    Hello.py tiedostoa on muokattu

--- examples/hello.py
+++ examples/hello.py
@@ -1,11 +1,4 @@
-# import a datetime libray to access current date and time
-import datetime
-
-# declare a variable named 'msg' and set some text into it
-msg = "Tämän ohjelman suoritti Jani Immonen"
-
-# print the message into console
-print(msg)
-
-# print the current date and time
-print(datetime.datetime.now())
+# read text from console and echo back
+text = input('Kirjoita jotain tekstiä: ')
+print("Kirjoitit: ")
+print(text)
