8b75af81e7e9a56ffbeb3aeadc0657041537f77d
Arttu Konttila <AB6254@student.jamk.fi>
2021-11-20 12:29:18 +0200

    Tehtävä L14T03 korjattu

--- Tehtävät/L14 poikkeukset/Tehtävä03.py
+++ Tehtävät/L14 poikkeukset/Tehtävä03.py
@@ -5,27 +5,30 @@ def isfloat(num):
         return True
     except ValueError:
         return False
+
 def isint(num):
     try:
         int(num)
         return True
     except ValueError:
         return False
+
 def isthiszero(num):
-    if(type(num) == str):
+    if(isfloat(num) == True or isint(num) == True):
         if(isfloat(num) == True):
             num = float(num)
             if(num == 0):
                 return True
+            elif(num != 0):
+                return False
         elif(isint(num) == True):
             num = int(num)
             if(num == 0):
                 return True
-        elif(isfloat(num) == True or isint(num) == True):
-            if(num != 0):
-                return False
-        else:
-            raise TypeError
+            elif(num != 0):
+                return False 
+    else:
+        raise TypeError
     
 try:
     print(isthiszero(input("Gibe number: ")))
